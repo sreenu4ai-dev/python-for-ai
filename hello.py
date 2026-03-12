834b632e49295823d436ee174708bfd5fe3fa875
sreenivas <sreenu4ai@gmail.com>
2026-03-12 08:19:27 +0530

    changes to commit first

---
+++ hello.py
@@ -0,0 +1,21 @@
+print("hello world")
+print("welcome world")
+
+age = 10
+name = "sreenu"
+
+print(age == 10)
+
+if age > 18:
+    print("you are an adult")
+else:
+    print("you are not an adult")
+
+peru ="jjj"
+
+peru ='vasu'
+
+print(peru)
+
+score =10
+print(score)
